4699309e0bbf72269e7716279acb5250665a04fe
LAPTOP-HAOF07OU\DIANA   KATHERINE <juandavidguarnizoc@gmail.com>
2023-11-24 23:42:26 -0500

    Se agrego el Codigo Java de Grafos en el que estan los public native para la compilacion y la creacion del .h

---
+++ Grafos.java
@@ -0,0 +1,9 @@
+package libgrafos;
+
+public class Grafos {
+
+    public native void createRoom(String Json);
+    public native Boolean assign_room(String Json);
+    public native void showRooms();
+
+}
